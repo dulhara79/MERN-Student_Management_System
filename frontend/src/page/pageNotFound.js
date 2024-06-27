49c98d7626325b7b306fe9c7e213d85d2f77d4d2
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-06-27 13:25:35 +0530

    Create pageNotFound.js

---
+++ frontend/src/page/pageNotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const pageNotFound = () => {
+  return (
+    <div>
+      <center>
+        <h1>Page Not Found</h1>
+        <br />
+        <h2>404 Error</h2>
+      </center>
+    </div>
+  );
+};
+
+export default pageNotFound;
